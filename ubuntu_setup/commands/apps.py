8e1a834e59205730d110b964dacd0ccd722b6e61
Andrew Yatsura <andrewyazura203@gmail.com>
2023-09-17 23:48:45 +0300

    remove docker desktop

--- ubuntu_setup/commands/apps.py
+++ ubuntu_setup/commands/apps.py
@@ -3,7 +3,6 @@ from os import path
 
 import click
 
-from ..apps.docker_desktop import install_docker_desktop
 from ..utils import echo_completion_message, run_command
 
 deb_files_option = click.option(
@@ -19,10 +18,6 @@ def install_apps(deb_files: str, **_) -> None:
     files = glob.glob(path.join(deb_files, "**/*.deb"), recursive=True)
 
     for file in files:
-        if "docker" in file.lower() and "desktop" in file.lower():
-            install_docker_desktop(file)
-            continue
-
         run_command(f"sudo apt install -y {file}")
 
     echo_completion_message("app install completed!")
